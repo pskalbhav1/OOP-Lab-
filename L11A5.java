1a3795a05838fb6acca1766d8d5f34287569341d
pskalbhav1 <praseedhakalbhavi@gmail.com>
2021-02-19 19:42:37 +0530

    Create L11A5.java

---
+++ L11A5.java
@@ -0,0 +1,30 @@
+class MyThreadl implements Runnable{
+    public void run(){
+        System.out.println("Thread "+Thread.currentThread().getName());
+        try{
+            Thread.sleep(1000);
+        }catch(InterruptedException e)
+        {
+            System.out.println(e);
+        }
+        System.out.println("Exit "+Thread.currentThread().getName());
+    }
+}
+public class L11A5 {
+    public static void main(String args[])
+    {
+        MyThreadl p=new MyThreadl();
+        Thread x=new Thread(p);
+        x.setName("First");
+        x.start();
+        
+        System.out.println(x.getName()+":"+x.getState());
+        MyThreadl px=new MyThreadl();
+        Thread xx=new Thread(px);
+        xx.setName("Second");
+        xx.start();
+        
+        System.out.println(xx.getName()+":"+xx.getState());
+    }
+    
+}
