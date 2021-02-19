5c0b0b2e8da103d97e64273000a2c3136a12a807
pskalbhav1 <praseedhakalbhavi@gmail.com>
2021-02-19 19:07:46 +0530

    Create L11R5_1.java

---
+++ L11R5_1.java
@@ -0,0 +1,47 @@
+class threadp extends Thread
+{
+  public void displayx(String msg)
+  {
+    System.out.print ("["+msg);
+    try
+    {
+      Thread.sleep(1000);
+    }
+    catch(InterruptedException e)
+    {
+      e.printStackTrace();
+    }
+    System.out.println ("]");
+  }
+  String msg;
+  threadp fobj;
+  threadp()
+  {
+      //
+  }
+  threadp(threadp p,String str)
+  {
+      fobj=p;
+    msg = str;
+    start();
+  }
+  public void run()
+  {
+      synchronized(fobj)
+      {
+          fobj.displayx(msg);
+      }
+  }
+}
+public class NewClassL11R5_1 {
+    public static void main(String args[])
+    {
+        threadp fnew=new threadp();
+     
+    threadp ss = new threadp(fnew, "welcome");
+    threadp ss1= new threadp(fnew,"new");
+    threadp ss2 = new threadp(fnew, "programmer");
+    
+    }
+    
+}
