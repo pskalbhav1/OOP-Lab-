ecdfdef04833f6ce39e4c279208db455ec75746f
pskalbhav1 <praseedhakalbhavi@gmail.com>
2021-02-20 14:25:39 +0530

    Create L1A3.java

---
+++ L1A3.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class L1A3 {
+public static void main(String args[]) {
+Scanner in = new Scanner(System.in);
+int s;
+s = in.nextInt();
+int i;
+for(i=1;i<=10;i++)
+{
+System.out.println(i*s); // Printing the multiplication table of s
+}
+}
+}
